2c3e595ebc415fd31beedb090afe7b15fdd04ec1
zhouzhongqing <zhouzhongqing@qq.com>
2026-05-30 20:18:28 +0800

    refactor:极端情况

--- src/main/java/com/zzq/task/ResendPendingTask.java
+++ src/main/java/com/zzq/task/ResendPendingTask.java
@@ -70,7 +70,7 @@ public class ResendPendingTask {
                             RedisStreamConfig.STREAM_KEY,
                             RedisStreamConfig.GROUP_NAME,
                             MachineInfoUtil.getHostName(), // 变成当前兜底消费者的名字
-                            Duration.ofSeconds(30), // 期望的 idle 时间再去强占
+                            Duration.ofSeconds(29), // 期望的 idle 时间再去强占
                             id
                     );
 
